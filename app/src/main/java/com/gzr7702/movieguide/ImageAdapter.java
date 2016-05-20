82a7ef6607362bc4f84ab4a8c325876ad48475d6
Rob Graziano <robgraziano@earthlink.net>
2016-05-20 12:38:21 -0400

    Initial commit

---
+++ app/src/main/java/com/gzr7702/movieguide/ImageAdapter.java
@@ -0,0 +1,63 @@
+package com.gzr7702.movieguide;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by rob on 5/4/16.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mImageUrl.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(500, 800));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(0, 0, 0, 0);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        //imageView.setImageResource(mThumbIds[position]);
+        Picasso.with(mContext)
+                .load(mImageUrl[position])
+                .into(imageView);
+        return imageView;
+    }
+
+    // references to our images
+    // Random internet images for now
+    private String[] mImageUrl = {
+            "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/1/1e/Large_Siamese_cat_tosses_a_mouse.jpg",
+            "http://cdn.grumpycats.com/wp-content/uploads/2016/02/12654647_974282002607537_7798179861389974677_n-758x758.jpg",
+            "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg"
+    };
+}
