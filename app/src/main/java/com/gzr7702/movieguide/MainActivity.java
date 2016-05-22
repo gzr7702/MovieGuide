f1e29acd949cf9465f6932ecaf8c6d2f45580070
Rob Graziano <robgraziano@earthlink.net>
2016-05-21 20:38:30 -0400

    fix settings

--- app/src/main/java/com/gzr7702/movieguide/MainActivity.java
+++ app/src/main/java/com/gzr7702/movieguide/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gzr7702.movieguide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -46,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
