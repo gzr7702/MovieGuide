b532e16aa18f9f6bd86ee09a0c7d339e3723fe2c
Rob Graziano <robgraziano@earthlink.net>
2016-05-22 14:12:23 -0400

    Added basic detail activity and fragment

---
+++ app/src/main/java/com/gzr7702/movieguide/DetailFragment.java
@@ -0,0 +1,29 @@
+package com.gzr7702.movieguide;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DetailFragment extends Fragment {
+
+    public DetailFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        return rootView;
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
