7e554c453b4dd03d165f91b2f0d345b655fc9f5f
Rob Graziano <robgraziano@earthlink.net>
2016-05-24 12:59:29 -0400

    Polished detail layout

--- app/src/main/java/com/gzr7702/movieguide/MainActivity.java
+++ app/src/main/java/com/gzr7702/movieguide/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                Toast.makeText(getApplicationContext(), "" + position,
-                        Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(getApplicationContext(), DetailActivity.class));
             }
         });
