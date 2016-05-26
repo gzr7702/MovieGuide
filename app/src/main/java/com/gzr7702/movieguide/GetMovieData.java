0424dd2ef1ce019c3f5f2050ef5e5f4bfb9def66
Rob Graziano <robgraziano@earthlink.net>
2016-05-25 20:20:10 -0400

    Added Class to get movie data

---
+++ app/src/main/java/com/gzr7702/movieguide/GetMovieData.java
@@ -0,0 +1,90 @@
+
+package com.gzr7702.movieguide;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GetMovieData extends AsyncTask<String, Void, Void> {
+
+    private final String LOG_TAG = GetMovieData.class.getSimpleName();
+
+    @Override
+    protected Void doInBackground(String... params) {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        String movieJSON = null;
+
+        try {
+            // Construct the URL
+            //"http://api.themoviedb.org/3/movie/popular?api_key=asdfasdf;
+            final String BASE_URL = "http://api.themoviedb.org/3/movie/";
+
+            final String QUERY_TYPE = params[0];
+            final String APPID_PARAM = null; //Grab the key from another file
+
+            Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                    .appendPath(QUERY_TYPE)
+                    .appendQueryParameter(APPID_PARAM, Info.getKey())
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Add end of line for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+
+            //implement getDataFromJson ===============================================================
+            //jsonStr = buffer.toString();
+            //getDataFromJson(jsonStr, locationQuery);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error ", e);
+        //} catch (JSONException e) {
+        //    Log.e(LOG_TAG, e.getMessage(), e);
+        //    e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
+
+        return null;
+    }
+}
