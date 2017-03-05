411ed8fbc4aeda35c431773f833a8784bf2ee7be
gzr7702 <rgraziano.gzr7702@gmail.com>
2017-03-05 08:42:59 -0500

    fix: remove tablet functionality

--- app/src/main/java/com/gzr7702/movieguide/MainActivity.java
+++ app/src/main/java/com/gzr7702/movieguide/MainActivity.java
@@ -3,26 +3,16 @@ package com.gzr7702.movieguide;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/*
-    * Main entry point to the application, holds MovieFragment
- */
-
 public class MainActivity extends AppCompatActivity {
-    private boolean mTwoPane;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        if (findViewById(R.id.movie_detail_container) != null) {
-            mTwoPane = true;
-            if (savedInstanceState == null) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.movie_detail_container, new DetailFragment())
-                        .commit();
-            }
-        } else {
-            mTwoPane = false;
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MovieFragment())
+                    .commit();
         }
     }
 
